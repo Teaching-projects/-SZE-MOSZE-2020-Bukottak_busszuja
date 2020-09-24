1471557abc797b68524211c24493b289521897f5
tmate93 <tr.mate93@gmail.com>
2020-09-24 13:28:48 +0200

    Add file handling to the program

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,8 @@
 #include <stdexcept>
 #include <string>
 
-int sti(char* c) {
+int sti(char* c)
+{
     std::string arg = c;
     try {
         std::size_t pos;
@@ -20,28 +21,39 @@ int sti(char* c) {
 }
 
 void kiir(const Szorny &s) {
-    std::cout << s.getName() << ": HP: " << s.getHp() << ", DMG: " << s.getDmg() << std::endl;
+	std::cout << s.getName() << ": HP: " << s.getHp() << ", DMG: " << s.getDmg() << std::endl;
 }
 
 int main(int argc, char **argv) {
-    Szorny s1(argv[1], sti(argv[2]), sti(argv[3]));
-    Szorny s2(argv[4], sti(argv[5]), sti(argv[6]));
-    std::cout << s1.getName() << " " << s1.getHp() << " " << s1.getDmg() << " " << s2.getName() << " " << s2.getHp() << " " << s2.getDmg() << std::endl;
+    Szorny s1;
+    Szorny s2;
+    try {
+        std::ifstream f1, f2;
+        f1.open(argv[1]);
+        f2.open(argv[2]);
+        if (!f1.good() || !f2.good())
+            throw 56;
+        else {
+            s1.parseUnit(f1);
+            s2.parseUnit(f2);
+            std::cout << s1.getName() << " " << s1.getHp() << " " << s1.getDmg() << " " << s2.getName() << " " << s2.getHp() << " " << s2.getDmg() << std::endl;
 
-    while (s1.getHp() > 0 && s2.getHp() > 0) {
-        std::cout << s1.getName() << " -> " << s2.getName() << std::endl;
-        s1.tamad(s2);
-        kiir(s1);
-        kiir(s2);
-        if (s1.getHp() > 0 && s2.getHp() > 0) {
-            std::cout << s2.getName() << " -> " << s1.getName() << std::endl;
-            s2.tamad(s1);
-            kiir(s1);
-            kiir(s2);
+            while (s1.getHp() > 0 && s2.getHp() > 0) {
+                s1.tamad(s2);
+                if (s1.getHp() > 0 && s2.getHp() > 0) {
+                    s2.tamad(s1);
+                }
+            }
+            if (s1.getHp() == 0) std::cout << s2.getName() << " wins. Remained HP: " << s2.getHp() << std::endl;
+            if (s2.getHp() == 0) std::cout << s1.getName() << " wins. Remained HP: " << s1.getHp() << std::endl;
+
+            system("pause");
+            return 0;
+
         }
-    }
-    if (s1.getHp() == 0) std::cout << s1.getName() << " meghalt. " << s2.getName() << " nyert." << std::endl;
-    if (s2.getHp() == 0) std::cout << s2.getName() << " meghalt. " << s1.getName() << " nyert." << std::endl;
+    } catch (int e) {
+        std::cout << "Nem letezo fajl lett megadva!" << std::endl;
 
-    return 0;
+    }
 }
+
--- Szorny.h
+++ Szorny.h
@@ -1,19 +1,22 @@
 #pragma once
 #include<iostream>
 #include<string>
-
+#include<fstream>
+#include<vector>
 
 class Szorny
 {
 public:
-	Szorny(std::string nev, int hp, int dmg):nev(nev),hp(hp),dmg(dmg) {}
+	Szorny(std::string nev, int hp, int dmg) :nev(nev), hp(hp), dmg(dmg) {}
+	Szorny(){}
 	int getDmg()const;
 	int getHp()const;
 	std::string getName()const;
 	void tamad(Szorny &)const;
+	void parseUnit(std::ifstream &);
 private:
-    const std::string nev;
+	std::string nev;
 	int hp;
-	const int dmg;
+	int dmg;
 
 };

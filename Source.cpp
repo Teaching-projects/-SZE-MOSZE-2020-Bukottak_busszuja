d0422082a94ae6e196f0d96e709989c2b4f1e024
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-10-27 21:56:16 +0100

    proba5

--- Source.cpp
+++ Source.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char **argv) {
 	try {
-		std::ifstream f(argv[1]);
-		Jsonparser f1(argv[2]);
+		Jsonparser f1(argv[1]);
+		std::ifstream f(argv[2]);
 		Jsonparser f2(f);
-		Szorny s2 = Szorny::parseUnit(f1);
-		Kalandor s1(Szorny::parseUnit(f2));
+		Szorny s2 = Szorny::parseUnit(f2);
+		Kalandor s1(Szorny::parseUnit(f1));
         Szorny::harc(s1, s2);
         if (s1.getHp() == 0) std::cout << s2.getName() << " wins. Remaining HP: " << s2.getHp() << std::endl;
         if (s2.getHp() == 0) std::cout << s1.getName() << " wins. Remaining HP: " << s1.getHp() << ", current level: " << s1.getLvl() << ", current experience: " << s1.getXp() << std::endl;

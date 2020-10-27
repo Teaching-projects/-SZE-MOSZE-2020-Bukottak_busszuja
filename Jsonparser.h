4d6a893fbf03da2926712019215610ac81ab3f29
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-10-27 23:13:38 +0100

    probautolsoma

--- Jsonparser.h
+++ Jsonparser.h
@@ -11,7 +11,7 @@ class Jsonparser {
 public:
 	Jsonparser(std::ifstream &);
 	Jsonparser(std::string& szoveg);
-	Jsonparser(std::string);
+	Jsonparser(const char *);
 	~Jsonparser();
 	std::string getErtek(const std::string &);
 private:

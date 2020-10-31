022fbf5baae214bf4391b78a9fba376fe711ba6e
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-10-31 16:44:04 +0100

    unit41

--- tests/test.cpp
+++ tests/test.cpp
@@ -89,6 +89,12 @@ TEST(Maintest, Tobb_kevesebb_adat_test) {
 	}
 }
 
+TEST(Szornytest, Lko_test) {
+	EXPECT_EQ(round(gcd(2.4,1.6)),round(0.8));
+	EXPECT_EQ(round(gcd(2.2, 4.4)), round(2.2));
+	EXPECT_EQ(round(gcd(1.3, 1.1)), round(0.1));
+}
+
 
 int main(int argc, char ** argv) {
 	::testing::InitGoogleTest(&argc, argv);

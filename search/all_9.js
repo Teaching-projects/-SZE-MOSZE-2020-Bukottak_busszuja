f0d24b91f1d8c3112db38a5d4075b51eea4f8b65
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-11-04 10:45:24 +0000

    Update gh-pages to output generated at e773e7e

--- search/all_9.js
+++ search/all_9.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['speed',['speed',['../classSzorny.html#a9dfbaf820c8c5c404b718fcd9f27b866',1,'Szorny']]],
-  ['szorny',['Szorny',['../classSzorny.html',1,'Szorny'],['../classSzorny.html#a92aae95371cc46713460468311778d6d',1,'Szorny::Szorny()']]],
-  ['szörny',['Szörny',['../classSz_xC3_xB6rny.html',1,'']]]
+  ['szorny',['Szorny',['../classSzorny.html',1,'Szorny'],['../classSzorny.html#a92aae95371cc46713460468311778d6d',1,'Szorny::Szorny()']]]
 ];
--- search/classes_2.js
+++ search/classes_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['szorny',['Szorny',['../classSzorny.html',1,'']]],
-  ['szörny',['Szörny',['../classSz_xC3_xB6rny.html',1,'']]]
+  ['szorny',['Szorny',['../classSzorny.html',1,'']]]
 ];

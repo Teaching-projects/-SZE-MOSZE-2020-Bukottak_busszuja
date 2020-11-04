e947dae2e73825f57fc5faea853d057ddb709b89
tmate93 <tr.mate93@gmail.com>
2020-11-04 16:18:54 +0100

    fix unit test

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,33 +3,32 @@
 
 
 
-
 TEST(Jsontest, Fajlvaltozo) {
 	std::ifstream f("Hosarkany.json");
 	JSON beolvasas = JSON::parseFromFile(f);
-	EXPECT_EQ(beolvasas.getErtek("name"), "Hosarkany");
-	EXPECT_EQ(beolvasas.getErtek("health_points"), "300");
-	EXPECT_EQ(beolvasas.getErtek("damage"), "30");
-	EXPECT_EQ(beolvasas.getErtek("attack_cooldown"),"2.4");
+	EXPECT_EQ(beolvasas.get<std::string>("name"), "Hosarkany");
+	EXPECT_EQ(beolvasas.get<std::string>("health_points"), "300");
+	EXPECT_EQ(beolvasas.get<std::string>("damage"), "30");
+	EXPECT_EQ(beolvasas.get<std::string>("attack_cooldown"), "2.4");
 }
 
 TEST(Jsontest, Szoveg) {
 	std::string szoveg = "Sotetvarazslo.json";
 	JSON beolvasas = JSON::parseFromFile(szoveg);
-	EXPECT_EQ(beolvasas.getErtek("name"), "Sotetvarazslo");
-	EXPECT_EQ(beolvasas.getErtek("health_points"), "250");
-	EXPECT_EQ(beolvasas.getErtek("damage"), "40");
-	EXPECT_EQ(beolvasas.getErtek("attack_cooldown"),"2.0");
+	EXPECT_EQ(beolvasas.get<std::string>("name"), "Sotetvarazslo");
+	EXPECT_EQ(beolvasas.get<std::string>("health_points"), "250");
+	EXPECT_EQ(beolvasas.get<std::string>("damage"), "40");
+	EXPECT_EQ(beolvasas.get<std::string>("attack_cooldown"), "2");
 }
 
 
 TEST(Jsontest, Fajlnev) {
-        const char * fajlnev="Arnykiraly.json";
+	const char * fajlnev = "Arnykiraly.json";
 	JSON beolvasas = JSON::parseFromFile(fajlnev);
-	EXPECT_EQ(beolvasas.getErtek("name"), "Arnykiraly");
-	EXPECT_EQ(beolvasas.getErtek("health_points"), "200");
-	EXPECT_EQ(beolvasas.getErtek("damage"), "48");
-	EXPECT_EQ(beolvasas.getErtek("attack_cooldown"),"2.3");
+	EXPECT_EQ(beolvasas.get<std::string>("name"), "Arnykiraly");
+	EXPECT_EQ(beolvasas.get<std::string>("health_points"), "200");
+	EXPECT_EQ(beolvasas.get<std::string>("damage"), "48");
+	EXPECT_EQ(beolvasas.get<std::string>("attack_cooldown"), "1.6");
 }
 
 
@@ -39,6 +38,6 @@ TEST(Jsontest, Fajlnev) {
 
 
 int main(int argc, char ** argv) {
-		::testing::InitGoogleTest(&argc, argv);
-		return RUN_ALL_TESTS();
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }

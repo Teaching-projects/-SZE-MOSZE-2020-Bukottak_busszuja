f4c66ef2af569f89b16911065b8d2c60c0472868
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-11-08 18:49:38 +0100

    fixunit23

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,20 +3,6 @@
 #include "Hero.h"
 #include "gtest/gtest.h"
 
-const std::map<int,std::string> error_messages = {
-    { 1 , "Bad number of arguments. Only a single scenario file should be provided." },
-    { 2 , "The provided scenario file is not accessible." },
-    { 3 , "The provided scenario file is invalid." },
-    { 4 , "JSON parsing error." }
-};
-
-void bad_exit(int exitcode){
-    std::cerr
-        << (error_messages.count(exitcode) ? error_messages.at(exitcode) : "Unknown error")
-        << std::endl;
-    exit(exitcode);
-}
-
 
 TEST(Jsontest, Fajlvaltozo) {
 	std::ifstream f("Hosarkany.json");
@@ -64,16 +50,9 @@ TEST(Jsontest, Whitespace_test) {
 	EXPECT_EQ(beolvasas.get<double>("attack_cooldown"), 1.7);
 }
 
-TEST(Maintest, Nem_letezo_fajl_test) {
-	std::string vart = "The provided scenario file is not accessible.";
-	testing::internal::CaptureStdout();
-	std::ifstream file;
-   	file.open("Lathatatlan.json");
-   	if(!file.good())bad_exit(2);
-	std::string output = testing::internal::GetCapturedStdout();
-	EXPECT_EQ(vart, output);
+TEST(Exceptiontest,Nem_letezo_fajl_test){
+    ASSERT_THROW(JSON::parseFromFile("Lathatatlan.json"), JSON::ParseException);
 }
-
 
 
 int main(int argc, char ** argv) {

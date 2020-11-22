244e0b32b4f258dd698231e91df957e51036c3e8
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-11-22 14:00:49 +0100

    fixdefense8

--- tests/test.cpp
+++ tests/test.cpp
@@ -121,6 +121,14 @@ EXPECT_EQ(tipush, "Hero");
 EXPECT_EQ(tipusm, "Monster");
 }
 
+TEST(Unittest,Hero_parse_test){
+Hero hero{Hero::parse("Dark_Wanderer.json")};
+Hero hos("Dark_Wanderer",30,3,1,1.1,20,5,1,1,0.9);
+EXPECT_TRUE(hero==hero1);
+}
+
+
+
 
 
 

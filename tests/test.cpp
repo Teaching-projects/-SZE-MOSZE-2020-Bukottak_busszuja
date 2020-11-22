3f4b7e6ec89f1a3839d81ef0902369b00ef1bc27
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-11-22 12:29:08 +0100

    fixdefense1

--- tests/test.cpp
+++ tests/test.cpp
@@ -10,6 +10,7 @@ TEST(Jsontest, Fajlvaltozo) {
 	EXPECT_EQ(beolvasas.get<std::string>("name"), "Hosarkany");
 	EXPECT_EQ(beolvasas.get<int>("health_points"), 300);
 	EXPECT_EQ(beolvasas.get<int>("damage"), 30);
+	EXPECT_EQ(beolvasas.get<int>("defense"), 3);
 	EXPECT_EQ(beolvasas.get<double>("attack_cooldown"), 2.4);
 }
 
@@ -18,6 +19,7 @@ TEST(Jsontest, Szoveg) {
 	JSON beolvasas = JSON::parseFromFile(szoveg);
 	EXPECT_EQ(beolvasas.get<std::string>("name"), "Sotetvarazslo");
 	EXPECT_EQ(beolvasas.get<int>("health_points"), 250);
+	EXPECT_EQ(beolvasas.get<int>("defense"), 1);
 	EXPECT_EQ(beolvasas.get<int>("damage"), 40);
 	EXPECT_EQ(beolvasas.get<double>("attack_cooldown"), 2.0);
 }
@@ -29,111 +31,11 @@ TEST(Jsontest, Fajlnev) {
 	EXPECT_EQ(beolvasas.get<std::string>("name"), "Arnykiraly");
 	EXPECT_EQ(beolvasas.get<int>("health_points"), 200);
 	EXPECT_EQ(beolvasas.get<int>("damage"), 48);
+	EXPECT_EQ(beolvasas.get<int>("defense"), 2);
 	EXPECT_EQ(beolvasas.get<double>("attack_cooldown"), 1.6);
 }
 
-TEST(Jsontest, Kulcssorrend_test) {
-	std::ifstream f("Kardvadasz.json");
-	JSON beolvasas = JSON::parseFromFile(f);
-	EXPECT_EQ(beolvasas.get<std::string>("name"), "Kardvadasz");
-	EXPECT_EQ(beolvasas.get<int>("health_points"), 280);
-	EXPECT_EQ(beolvasas.get<int>("damage"), 48);
-	EXPECT_EQ(beolvasas.get<double>("attack_cooldown"), 2.1);
-}
 
-TEST(Jsontest, Whitespace_test) {
-	std::ifstream f("Obelisk.json");
-	JSON beolvasas = JSON::parseFromFile(f);
-	EXPECT_EQ(beolvasas.get<std::string>("name"), "Obelisk");
-	EXPECT_EQ(beolvasas.get<int>("damage"), 45);
-	EXPECT_EQ(beolvasas.get<int>("health_points"), 260);
-	EXPECT_EQ(beolvasas.get<double>("attack_cooldown"), 1.7);
-}
-
-TEST(Exceptiontest,Nem_letezo_fajl_test){
-    ASSERT_THROW(JSON::parseFromFile("Lathatatlan.json"), JSON::ParseException);
-}
-
-TEST(Exceptiontest,Nincs_hibauzenet_test){
-    ASSERT_NO_THROW(Hero::parse("Langpallos.json"));
-    ASSERT_NO_THROW(Monster::parse("Hosarkany.json"));
-}
-
-TEST(Unittest,Monster_parse_test){
-Monster monster{Monster::parse("Sotetvarazslo.json")};
-Monster monster1("Sotetvarazslo", 250, 40, 2.0);
-EXPECT_TRUE(monster==monster1);
-}
-
-TEST(Unittest,Hero_parse_test){
-Hero hero{Hero::parse("Langpallos.json")};
-Hero hero1("Langpallos", 180, 11, 9.1, 100, 3, 2, 0.8,0,1);
-EXPECT_TRUE(hero==hero1);
-}
-
-TEST(Unittest,Badscenario_exception_test){
-std::string vart = "The provided scenario file is invalid.";
-testing::internal::CaptureStdout();
-JSON scenario = JSON::parseFromFile("badscenario.json");
-if (!(scenario.count("hero") && scenario.count("monsters")))std::cout << "The provided scenario file is invalid.";
-std::string output = testing::internal::GetCapturedStdout();
-EXPECT_EQ(vart, output);
-}
-
-TEST(Unittest, Hero_Monster_different_test) {
-Hero hero {Hero::parse("Langpallos.json")};
-Monster monster{Monster::parse("Hosarkany.json")};
-std::string tipush = typeid(hero).name();
-std::string tipusm = typeid(monster).name();
-tipush.erase(0, 1);
-tipusm.erase(0, 1);
-EXPECT_EQ(tipush, "Hero");
-EXPECT_EQ(tipusm, "Monster");
-}
-
-TEST(Unittest,Fight_function_test){
-Hero hero {Hero::parse("Langpallos.json")};
-Monster monster{Monster::parse("Hosarkany.json")};
-hero.fightTilDeath(monster);
-EXPECT_TRUE(monster.isAlive());
-EXPECT_FALSE(hero.isAlive());
-}
-
-TEST(Unittest,Private_functions_test){
-Hero hero("Langpallos", 180, 11, 9.1, 100, 3, 2, 0.8, 90, 1);
-Monster monster{Monster::parse("Hosarkany.json")};
-Monster * mptr = &monster;
-hero.tamad(mptr);
-Hero hero1("Langpallos", 183, 13, 7.28, 100, 3, 2, 0.8, 1, 2);
-EXPECT_TRUE(hero1==hero);
-}
-
-TEST(Jsontest,Different_input_equas_test){
-	std::ifstream f("Hosarkany.json");
-	std::string szoveg = "Hosarkany.json";
-	const char * fajlnev = "Hosarkany.json";
-	JSON beolvasas1 = JSON::parseFromFile(f);
-	JSON beolvasas2 = JSON::parseFromFile(szoveg);
-	JSON beolvasas3 = JSON::parseFromFile(fajlnev);
-	EXPECT_EQ(beolvasas1.get<std::string>("name"), beolvasas2.get<std::string>("name"));
-	EXPECT_EQ(beolvasas3.get<std::string>("name"), beolvasas2.get<std::string>("name"));
-	EXPECT_EQ(beolvasas1.get<int>("health_points"), beolvasas2.get<int>("health_points"));
-	EXPECT_EQ(beolvasas3.get<int>("health_points"), beolvasas2.get<int>("health_points"));
-	EXPECT_EQ(beolvasas1.get<int>("damage"),beolvasas2.get<int>("damage"));
-	EXPECT_EQ(beolvasas3.get<int>("damage"),beolvasas2.get<int>("damage"));
-	EXPECT_EQ(beolvasas1.get<double>("attack_cooldown"),beolvasas2.get<double>("attack_cooldown"));
-	EXPECT_EQ(beolvasas3.get<double>("attack_cooldown"),beolvasas2.get<double>("attack_cooldown"));
-}
-
-TEST(Unittest,Type_fine_test){
-Monster monster{Monster::parse("Hosarkany.json")};
-std::string type_hp = typeid(monster.getHealthPoints()).name();
-std::string type_dmg = typeid(monster.getDamage()).name();
-std::string type_speed = typeid(monster.getAttackCoolDown()).name();
-EXPECT_TRUE(type_hp == "i");
-EXPECT_TRUE(type_dmg == "i");
-EXPECT_TRUE(type_speed == "d");
-}
 
 
 int main(int argc, char ** argv) {

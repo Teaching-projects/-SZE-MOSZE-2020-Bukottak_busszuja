39f679ab170cc13c4e64fa523775a65a15654d8a
tmate93 <tr.mate93@gmail.com>
2020-12-05 18:23:01 +0100

    Update Source.cpp
    try fix error

--- Source.cpp
+++ Source.cpp
@@ -93,11 +93,11 @@ void testMode() {
     dhero.magical = 1;
     dmonster.physical = 0;
     dmonster.magical = 0;
-    Game jatek("markedmap.txt");
-    MarkedMap palya("markedmap.txt");
+    //Game jatek("markedmap.txt");
+    MarkedMap jatek("markedmap.txt");
     Hero hos("Prince Aidan of Khanduras", 30, dhero, 1, 1.1, 20, 5, 1, 1, 1, 0.9, 1, 1);
     Monster monster1("Training Dummy", 250, dmonster, 1, 2.0);
-    jatek.addMarkedMapunits(palya,hos,monster1);
+    jatek.addMarkedMapunits(jatek,hos,monster1);
     jatek.run();
 }
 
